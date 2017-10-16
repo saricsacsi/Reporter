d35af3cb0d05ea2be5f7420dd4eb1823b14c4b75
saricsacsi <saricsacsi@users.noreply.github.com>
2017-10-16 13:21:45 +0200

    last version here

--- Reporter_Zeppelin/README.md
+++
@@ -1,5 +0,0 @@
-# ChangeICO
-
-This is the smart contract for the ChangeICO token and tokensale.
-
-Using Truffle and open-zeppelin
--- Reporter_Zeppelin/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-var ChangeCoinPresale = artifacts.require("ChangeCoinPresale.sol")
-
-module.exports = function(deployer, network, accounts) {
-  deployer.deploy(ChangeCoinPresale)
-}
